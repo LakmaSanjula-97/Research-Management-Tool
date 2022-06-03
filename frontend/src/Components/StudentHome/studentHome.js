1037d58e8b6e092d5bc22c8ac98460583e1e6ba6
Lakma <lakmagamage97@gmail.com>
2022-06-04 02:30:10 +0530

    studentHome

---
+++ frontend/src/Components/StudentHome/studentHome.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+
+
+
+export default class StudentHome extends Component {
+
+    render() {
+        return (
+            <div>
+
+            
+
+            <section id='sec5'>
+                <div>
+                    <h1 id='text-home1'>xxxxxxxxxxxxxxxxxx -2022</h1>
+                </div>
+                <div>
+                    <h1 id='text-home1'>SLIIT</h1>
+                    <Link className="login-btn" to="/studentNav/ViewCoSupervisor">lkxkncjfhvu</Link>
+                </div>
+            </section>
+
+
+
+            </div>
+        );
+    }
+}

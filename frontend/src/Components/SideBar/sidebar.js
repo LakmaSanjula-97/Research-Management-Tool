b5c281c303fbc0aca6817d4940aae625db91c5a1
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2022-06-04 02:12:54 +0530

    side bar change

--- frontend/src/Components/SideBar/sidebar.js
+++ frontend/src/Components/SideBar/sidebar.js
@@ -73,11 +73,11 @@ function sidebar() {
                 </ul>
 
                 <li data-toggle="collapse" data-target="#journey" className="collapsed">
-                  <a href="#"><i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;&nbsp; Journey <span className="arrow"></span></a>
+                  <a href="#"><i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;&nbsp; Supervisor Request <span className="arrow"></span></a>
                 </li>
                 <ul className="sub-menu collapse" id="journey">
-                  <li><a href="/dashboard/addJourney">Add Journey</a></li>
-                  <li><a href="/dashboard/viewJourney">All Journeys</a></li>
+                  <li><a href="/dashboard/AddRequestSupervisor">Add Supervisor</a></li>
+                  <li><a href="/dashboard/ViewSupervisorApproval">View Supervisor status</a></li>
                 </ul>
 
                 <li data-toggle="collapse" data-target="#driver" className="collapsed">

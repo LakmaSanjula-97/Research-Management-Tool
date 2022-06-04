09ce045b10d952b2db4673ae3a42603f8c5aec00
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2022-06-04 08:20:08 +0530

    group register test

---
+++ frontend/src/Components/GroupRegister/GroupRegister.test.js
@@ -0,0 +1,10 @@
+import {render} from "@testing-library/react";
+import GroupRegister from './ViewGroupDetails';
+
+describe("Search Input Component", () => {
+    it("rendered input", () => {
+        const { getByTestId } = render(<GroupRegister/>);
+        const input = getByTestId("searchbar");
+        expect(input).toBeTruthy();
+    })
+});
